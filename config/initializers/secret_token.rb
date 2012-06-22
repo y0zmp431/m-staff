9faa6a0177692023216f103027952bcd6760778c
ilp <ilp@thinkpad.home>
2012-06-22 17:03:01 +0300

    First init commit; Work authlogic;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Base::Application.config.secret_token = '43c609ebf9f84b514f835f0f49d17f486d93a61d66bc60332e18787c1d466a3bfc253fc018d902e3e8ec5447f3d782d7c3704c9d3014049824802c8b79f34da5'
