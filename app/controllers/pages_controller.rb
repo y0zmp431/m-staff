af0a05ff4a02244aec5ed007214ec4ea8445f8a6
Levanevskii-Perevozchikov I.V <ilp416@gmail.com>
2013-09-19 10:33:27 +0300

    Add index controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def index
+		@banner = Banner.where(:banner_type => 'big_banner').first(:offset => rand(Banner.where(:banner_type => 'big_banner').count))
   end
 
   def test
