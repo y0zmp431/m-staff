5aff657360ea8d443d974f0acd5aab6c574a838c
Levanevskii-Perevozchikov I.V <ilp416@gmail.com>
2014-03-18 11:56:13 +0300

    Fix article factory

--- test/factories/articles.rb
+++ test/factories/articles.rb
@@ -7,7 +7,9 @@ FactoryGirl.define do
 		title "index"
 		text "index page" 
 		published "true"
-    user
+    before(:create) do |article|
+      article.user = User.find_by_login('writer') || FactoryGirl.create(:writer)
+    end
 	end
 
 end
