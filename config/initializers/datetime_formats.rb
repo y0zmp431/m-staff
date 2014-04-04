5b79786ecbdb09d90d61e44b2e3063cf4d660220
Levanevskii-Perevozchikov I.V <ilp416@gmail.com>
2014-04-04 13:04:26 +0300

    Add datetime formats

---
+++ config/initializers/datetime_formats.rb
@@ -0,0 +1,5 @@
+Time::DATE_FORMATS[:default] = "%d.%m.%Y %H:%M"
+Time::DATE_FORMATS[:short_text] = "%e %b %Y %H:%M"
+Time::DATE_FORMATS[:short_text_with_time] = "%e %b %Y %H:%M"
+Time::DATE_FORMATS[:text] = "%e %B %Y"
+Time::DATE_FORMATS[:text_with_time] = "%e %B %Y %H:%M"
