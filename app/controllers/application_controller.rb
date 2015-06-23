cfdf37d0968f6ffa04062cdca44e6fb158dd9361
Levanevski-Perevozchikov Ilya <ilp416@gmail.com>
2015-06-23 20:48:56 +0300

    Add i18n menu; Add i18n method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user_session, :current_user
+  before_filter :set_locale
 
 	rescue_from CanCan::AccessDenied do |exception|
 		redirect_to root_url, :alert => exception.message
@@ -16,4 +17,15 @@ class ApplicationController < ActionController::Base
       return @current_user if defined?(@current_user)
       @current_user = current_user_session && current_user_session.user
     end
+
+    def set_locale
+      I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+    end
+
+    def extract_locale_from_subdomain
+      if parsed_locale = request.subdomains.first
+        I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+      end
+    end
+
 end
